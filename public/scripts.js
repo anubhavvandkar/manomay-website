0b3fda96c91dde5a09d8a49b0bc0bd15b3421332
Dhiraj03 <dhiraj.181co118@nitk.edu.in>
2022-05-06 18:58:56 +0530

    changes before bootstrap

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCqDV4tmv9Ma08Ga9WFflMqF2ccS8efePc",
+  authDomain: "manomay-74eb0.firebaseapp.com",
+  projectId: "manomay-74eb0",
+  storageBucket: "manomay-74eb0.appspot.com",
+  messagingSenderId: "523447708765",
+  appId: "1:523447708765:web:9cf02cddc71ffc1b32b1b4",
+  measurementId: "G-DTYBH18M1G"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function activeListener(event, tabName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("primary");
@@ -16,6 +32,16 @@ function activeListener(event, tabName) {
   }
   else{
     document.getElementById("footer").style.display = "table";
+  }2
+  scazrollTo(0,0);  
+}
+
+function responsive_navbar(x) {
+  x.classList.toggle("fa-times");
+  var y = document.getElementById("navbar");
+  if (y.className == "navbar") {
+    y.className += " responsive";
+  } else {
+    y.className = "navbar";
   }
-  scrollTo(0,0);
 }
